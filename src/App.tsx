52ebe5e0b5217e5da8cede439b7bd5c5eb57248f
wwnp <iamtodaysad@gmail.com>
2022-02-24 16:57:21 +0300

    rf

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Pagination, Container, TextField, Stack, Link } from '@mui/material';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+const BASE_URL = 'http://hn.algolia.com/api/v1/search?'
 
 function App() {
+  const [posts, setPosts] = useState<any[]>([])
+  const [query, setQuery] = useState('react')
+  const [page, setPage] = useState(1)
+  const [pageQty, setPageQty] = useState(0)
+  useEffect(() => {
+    axios.get(BASE_URL + `query=${query}&page=${page - 1}`).then(({ data }) => {
+      console.log(data)
+      setPosts(data.hits)
+      setPageQty(data.nbPages)
+    })
+  }, [query, page])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <TextField
+        fullWidth
+        label="query"
+        value={query}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
+      >
+      </TextField>
+      <Stack spacing={5}>
+        {!!pageQty && (
+          < Pagination
+            count={pageQty}
+            color="secondary"
+            page={page}
+            onChange={(_, num) => setPage(num)}
+          />
+        )}
+        {
+          posts.length
+            ? posts.map(post => {
+              return (
+                <Link
+                  key={post.objectID}
+                  href={post.url}
+                >
+                  {post.title}
+                </Link>
+              )
+            })
+            : <h1>123</h1>
+        }
+
+      </Stack>
+
+      <Button variant="contained">Text</Button>
+    </Container>
   );
 }
 
